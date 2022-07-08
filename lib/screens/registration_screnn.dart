d7f43aeae2f9a3cdfacb1cfe9d5cdbdf1a9f0911
ghisford <ghislainnzima@gmail.com>
2022-07-08 17:05:36 +0300

    final touches

--- lib/screens/registration_screnn.dart
+++ lib/screens/registration_screnn.dart
@@ -1,6 +1,5 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({Key? key}) : super(key: key);
@@ -36,11 +35,11 @@ class _RegisterViewState extends State<RegisterView> {
       body: Stack(
         children: <Widget>[
           //orange center
-const Positioned(
-          height: 706.8624877929688,
-          width: 481.7652893066406,
-          left: -70,
-          top: 0,
+          const Positioned(
+            height: 706.8624877929688,
+            left: 0,
+            right: 0,
+            top: 0,
             child: Image(
               image: AssetImage("assets/images/Vector 3.png"),
             ),
@@ -63,72 +62,132 @@ const Positioned(
             width: 426.5,
             left: -30.5,
             top: -10.5,
-            
-
             child: Image(
               image: AssetImage("assets/images/Vector 4.png"),
             ),
           ),
-          
-          Column(
-            children: [
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.2,
-              ),
-              const Text(
-                "Create ",
-                style: TextStyle(
-                  fontWeight: FontWeight.w600,
-                  fontSize: 30,
-                  color: Colors.white,
-                ),
-              ),
-             const Text(
-               " Account",
-               style: TextStyle(
-                 fontWeight: FontWeight.w600,
-                 fontSize: 30,
-                  color: Colors.white,
-               ),
-             ),
-              TextField(
-                autocorrect: false,
-                decoration: const InputDecoration(
-                  hintText: 'Name',
-                ),
-                controller: _name,
-              ),
-              TextField(
-                enableSuggestions: false,
-                autocorrect: false,
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(hintText: 'enter email'),
-                controller: _email,
-              ),
-              TextField(
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: false,
-                decoration: const InputDecoration(
-                  hintText: 'enter password',
-                ),
-                controller: _password,
-              ),
-              Row(
+          Positioned(
+            right: 5,
+            top: 40,
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.arrow_forward_ios),
+              color: Colors.white,
+            ),
+          ),
+
+          ListView(children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 30),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.15,
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    child: const Text(
+                      "Create Account",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 30,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.15,
+                  ),
+                  TextFormField(
+                    cursorColor: Colors.white,
+                    style: const TextStyle(
+                      color: Colors.white,
+                    ),
+                    autocorrect: false,
+                    decoration: const InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white)),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white)),
+                        hintText: 'Name',
+                        hintStyle: TextStyle(
+                          color: Colors.white,
+                        )),
+                    controller: _name,
+                  ),
+                  TextField(
+                    cursorColor: Colors.white,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white)),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white)),
+                        hintText: 'Email',
+                        hintStyle: TextStyle(
+                          color: Colors.white,
+                        )),
+                    controller: _email,
+                  ),
+                  TextField(
+                    cursorColor: Colors.white,
+                    obscureText: true,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    decoration: const InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white)),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white)),
+                        hintText: 'Password',
+                        hintStyle: TextStyle(
+                          color: Colors.white,
+                        )),
+                    controller: _password,
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.025,
+                  ),
+                  Row(
+                    children: [
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Sign up',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white,
+                            fontSize: 30,
+                          ),
+                        ),
+                      ),
+                      const Icon(
+                        Icons.arrow_forward,
+                        color: Colors.white,
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.045,
+                  ),
                   TextButton(
                     onPressed: () {},
-                    child: const Text('Sign up'),
-                  ),
-                  const Icon(Icons.forward)
+                    child: const Text(
+                      "Sign in",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  )
                 ],
               ),
-              TextButton(
-                onPressed: () {},
-                child: const Text("Sign in"),
-              )
-            ],
-          ),
+            ),
+          ]),
         ],
       ),
     );
